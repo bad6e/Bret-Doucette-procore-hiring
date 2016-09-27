f78751cf4ee63cc44596ae6da2cffc18c5c7679e
Bret Doucette <doucette.bret@gmail.com>
2016-09-27 17:38:22 -0600

    Added react logic

---
+++ app/assets/javascripts/components/_store.jsx
@@ -0,0 +1,11 @@
+var Store = React.createClass({
+  render : function() {
+    const { name, id, city } = this.props;
+    return (
+      <tr>
+        <td>{name}</td>
+        <td>{city}</td>
+      </tr>
+    );
+  }
+});
---
+++ app/assets/javascripts/components/_taco.jsx
@@ -0,0 +1,15 @@
+var Taco = React.createClass({
+  render : function() {
+    const { name, id, handleChange } = this.props;
+    return (
+      <div>
+        <input type="checkbox"
+               name={name}
+               value={id}
+               onChange={handleChange}
+        />
+               {name}
+      </div>
+    );
+  }
+});
