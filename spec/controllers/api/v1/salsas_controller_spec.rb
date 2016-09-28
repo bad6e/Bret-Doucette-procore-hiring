7977d067c05b244fde8d3f8d8297128e3c95d432
Bret Doucette <doucette.bret@gmail.com>
2016-09-28 13:32:18 -0600

    Added Get salsas spec

--- spec/controllers/api/v1/salsas_controller_spec.rb
+++ spec/controllers/api/v1/salsas_controller_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::SalsasController, type: :controller do
+   describe "GET /api/v1/interests" do
 
+    it "returns empty array if no Salsas/Sauces" do
+      get :index, format: :json
+      expect(response_data.length).to eq(0)
+      expect(response_data).to eq([])
+    end
+
+    it "returns all the Salsas/Sauces" do
+      salsa_one   = FactoryGirl.create(:salsa)
+      salsa_two   = FactoryGirl.create(:salsa)
+      salsa_three = FactoryGirl.create(:salsa)
+      salsa_four  = FactoryGirl.create(:salsa)
+
+      get :index, format: :json
+      expect(response_data.length).to eq(4)
+      expect(response_data[0][:name]).to eq(salsa_one.name)
+    end
+  end
 end
