a1c2e24a9698d0070f25b73f14ed6b84f29f8de3
Bret Doucette <doucette.bret@gmail.com>
2016-09-28 16:04:17 -0600

    Refactored search controller

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -1,26 +1,39 @@
 class Api::V1::SearchController < ApplicationController
   respond_to :json
+  before_action :verify_params_exist, only: [:index]
 
   def index
     if search_params[:tacos] && search_params[:salsas]
-      taco_ids = search_params[:tacos].split(',')
-      salsa_ids = search_params[:salsas].split(',')
-      stores = Store.find_tacos_by_ids(taco_ids).group_results(taco_ids.count) & Store.find_salsas_by_ids(salsa_ids).group_results(salsa_ids.count)
-      respond_with stores
+      stores = find_stores_by_taco_ids(search_params) & find_stores_by_salsa_ids(search_params)
     elsif search_params[:tacos] && !search_params[:salsas]
-      taco_ids = search_params[:tacos].split(',')
-      stores = Store.find_tacos_by_ids(taco_ids).group_results(taco_ids.count)
-      respond_with stores
-    elsif !search_params[:tacos] && search_params[:salsas]
-      salsa_ids = search_params[:salsas].split(',')
-      stores = Store.find_salsas_by_ids(salsa_ids).group_results(salsa_ids.count)
-      respond_with stores
+      stores = find_stores_by_taco_ids(search_params)
     else
-      respond_with ''
+      stores = find_stores_by_salsa_ids(search_params)
     end
+    respond_with stores
   end
 
   private
+
+    def find_stores_by_taco_ids(params)
+      ids = search_params[:tacos].split(',')
+      count = ids.length
+      Store.find_all_stores_with_specific_tacos(ids, count)
+    end
+
+    def find_stores_by_salsa_ids(params)
+      ids = search_params[:salsas].split(',')
+      count = ids.length
+      Store.find_all_stores_with_specific_salsas(ids, count)
+    end
+
+    def verify_params_exist
+      if search_params.empty?
+        render status: 400, json: {
+          error: "Must provide taco and/or salsa ids"
+        }.to_json
+      end
+    end
 
     def search_params
       params.permit(:salsas,
