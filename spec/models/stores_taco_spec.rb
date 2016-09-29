081a7775e1b890e224786b3f764fd2fecfdb6c61
Bret Doucette <doucette.bret@gmail.com>
2016-09-29 16:02:15 -0600

    Added additional model testing

--- spec/models/stores_taco_spec.rb
+++ spec/models/stores_taco_spec.rb
@@ -12,8 +12,20 @@ describe City do
   it { should validate_presence_of(:taco_id) }
   it { should validate_presence_of(:price) }
 
+  it { should validate_uniqueness_of(:taco_id).scoped_to(:store_id) }
+
   it { should belong_to(:store) }
   it { should belong_to(:taco) }
 
   it { should validate_numericality_of(:price) }
+
+  it "should only accept prices in the X.XX format - rounds down" do
+    stores_taco = FactoryGirl.create(:stores_taco, price: BigDecimal.new('3.51'))
+    expect(stores_taco.price).to eq(BigDecimal.new('3.51'))
+  end
+
+  it "should only accept prices in the X.XX format - rounds up" do
+    stores_taco = FactoryGirl.create(:stores_taco, price: BigDecimal.new('3.519'))
+    expect(stores_taco.price).to eq(BigDecimal.new('3.52'))
+  end
 end
