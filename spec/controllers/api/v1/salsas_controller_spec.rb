eb7e0088df16dfe93daee3a062c076af5aa59986
Bret Doucette <doucette.bret@gmail.com>
2016-09-29 16:13:58 -0600

    Refactored controller spec for salsa and taco

--- spec/controllers/api/v1/salsas_controller_spec.rb
+++ spec/controllers/api/v1/salsas_controller_spec.rb
@@ -2,23 +2,35 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::SalsasController, type: :controller do
    describe "GET /api/v1/salsas" do
+    context "there are no salsas" do
+      before(:each) do
+        get :index, format: :json
+      end
 
-    it "returns empty array if no Salsas/Sauces" do
-      get :index, format: :json
-      expect(response_data.length).to eq(0)
-      expect(response_data).to eq([])
+      it { should respond_with(200) }
+
+      it "returns empty array if there are no salsas/sauces" do
+        expect(response_data.length).to eq(0)
+        expect(response_data).to eq([])
+      end
     end
 
-    it "returns all the Salsas/Sauces" do
-      salsa_one   = FactoryGirl.create(:salsa)
-      salsa_two   = FactoryGirl.create(:salsa)
-      salsa_three = FactoryGirl.create(:salsa)
-      salsa_four  = FactoryGirl.create(:salsa)
+    context "there are tacos" do
+      before(:each) do
+        @salsa_one   = FactoryGirl.create(:salsa)
+        salsa_two   = FactoryGirl.create(:salsa)
+        salsa_three = FactoryGirl.create(:salsa)
+        @salsa_four  = FactoryGirl.create(:salsa)
+        get :index, format: :json
+      end
 
-      get :index, format: :json
-      expect(response_data.length).to eq(4)
-      expect(response_data[0][:name]).to eq(salsa_one.name)
-      expect(response_data[3][:name]).to eq(salsa_four.name)
+      it { should respond_with(200) }
+
+      it "returns all the salsas/sauces" do
+        expect(response_data.length).to eq(4)
+        expect(response_data[0][:name]).to eq(@salsa_one.name)
+        expect(response_data[3][:name]).to eq(@salsa_four.name)
+      end
     end
   end
 end
--- spec/controllers/api/v1/tacos_controller_spec.rb
+++ spec/controllers/api/v1/tacos_controller_spec.rb
@@ -1,24 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::TacosController, type: :controller do
-   describe "GET /api/v1/tacos" do
+  describe "GET /api/v1/tacos" do
+    context "there are no tacos" do
+      before(:each) do
+        get :index, format: :json
+      end
 
-    it "returns empty array if no tacos" do
-      get :index, format: :json
-      expect(response_data.length).to eq(0)
-      expect(response_data).to eq([])
+      it { should respond_with(200) }
+
+      it "returns empty array if no there are no tacos" do
+        expect(response_data.length).to eq(0)
+        expect(response_data).to eq([])
+      end
     end
 
-    it "returns all the tacos" do
-      taco_one   = FactoryGirl.create(:taco)
-      taco_two   = FactoryGirl.create(:taco)
-      taco_three = FactoryGirl.create(:taco)
-      taco_four  = FactoryGirl.create(:taco)
+    context "there are tacos" do
+      before(:each) do
+        @taco_one   = FactoryGirl.create(:taco)
+        taco_two    = FactoryGirl.create(:taco)
+        taco_three  = FactoryGirl.create(:taco)
+        @taco_four  = FactoryGirl.create(:taco)
+        get :index, format: :json
+      end
 
-      get :index, format: :json
-      expect(response_data.length).to eq(4)
-      expect(response_data[0][:name]).to eq(taco_one.name)
-      expect(response_data[3][:name]).to eq(taco_four.name)
+      it { should respond_with(200) }
+
+      it "returns all the tacos" do
+        expect(response_data.length).to eq(4)
+        expect(response_data[0][:name]).to eq(@taco_one.name)
+        expect(response_data[3][:name]).to eq(@taco_four.name)
+      end
     end
   end
 end
